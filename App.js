d1bc824dd6b27b67a558809458965c366c3e511d
Olukemi <kemijkf@gmail.com>
2021-12-29 21:44:39 -0500

    created login page and sign up page :)

--- App.js
+++ App.js
@@ -1,6 +1,9 @@
+import * as React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button, Image, } from 'react-native';
+import { StyleSheet, Text, View, Button, Image, TextInput, } from 'react-native';
 import { SafeAreaView, ScrollView, TouchableOpacity, useColorScheme, } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import AppLoading from 'expo-app-loading';
 import {
   useFonts,
@@ -10,6 +13,62 @@ import {
   Comfortaa_600SemiBold,
   Comfortaa_700Bold,
 } from '@expo-google-fonts/comfortaa';
+
+/* starting page -> this is what the user will first see when they download the app. they can sign up or log in */
+function StartingPage({ navigation }){
+  return (
+    <View style={styles.container}>
+      
+      <Text style={styles.name}>budgetMode</Text>
+
+      <Image style={styles.logo} source={require('./assets/budgetmodelogo.png')} />
+
+          <TouchableOpacity style={styles.loginButton} onPress={() => navigation.navigate('Login Page')}>
+          <Text style={styles.loginButtonText}>LOG IN</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={styles.signUpButton} onPress={() => navigation.navigate('Sign Up Page')}>
+          <Text style={styles.signUpButtonText}>SIGN UP</Text>
+          </TouchableOpacity>
+
+
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+/* login page -> the user will enter their email and password to login. this is only required once unless the user logs out of their account */
+function LoginPage({ navigation }) {
+  return (
+    <View style={styles.container2}>
+      <Text style ={styles.loginTitle}>Log In</Text>
+
+      <TextInput style ={styles.loginInput} underlineColorAndroid="transparent" placeholder='Email'/>
+      <TextInput style ={styles.loginInput} underlineColorAndroid="transparent" placeholder='Password'/>
+
+      <TouchableOpacity style={styles.loginButton2} onPress={() => navigation.navigate()}>
+      <Text style={styles.loginButtonText2}>LOG IN</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+function SignUpPage({navigation}) {
+  return (
+    <View style={styles.container2}>
+      <Text style ={styles.loginTitle}>Sign Up</Text>
+
+      <TextInput style ={styles.loginInput} underlineColorAndroid="transparent" placeholder='Email'/>
+      <TextInput style ={styles.loginInput} underlineColorAndroid="transparent" placeholder='Password'/>
+
+      <TouchableOpacity style={styles.loginButton2} onPress={() => navigation.navigate()}>
+      <Text style={styles.loginButtonText2}>SIGN UP</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   let [fontsLoaded, error] = useFonts ({
@@ -21,23 +80,13 @@ export default function App() {
   }
 
   return (
-    <View style={styles.container}>
-      
-      <Text style={styles.name}>budgetMode</Text>
-
-      <Image style={styles.logo} source={require('./assets/budgetmodelogo.png')} />
-
-          <TouchableOpacity style={styles.loginButton}>
-          <Text style={styles.loginButtonText}>LOG IN</Text>
-          </TouchableOpacity>
-
-          <TouchableOpacity style={styles.signUpButton}>
-          <Text style={styles.signUpButtonText}>SIGN UP</Text>
-          </TouchableOpacity>
-
-
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Starting Page" component={StartingPage} />
+        <Stack.Screen name="Login Page" component={LoginPage} />
+        <Stack.Screen name="Sign Up Page" component={SignUpPage} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
@@ -50,8 +99,8 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 
+  /* STARTING PAGE ---------------------------------*/
   name: {
-    marginTop: 80,
     fontSize: 40,
     fontFamily: 'Comfortaa_400Regular',
     marginBottom: 50,
@@ -60,37 +109,78 @@ const styles = StyleSheet.create({
   logo: {
     width: 260,
     height: 270,
+    marginBottom: 150,
   },
 
   loginButton: {
-    width: "80%",
+    width: "42%",
     borderRadius: 10,
-    height: 50,
+    height: 45,
     alignItems: "center",
     justifyContent: "center",
-    marginTop: 60,
-    marginBottom: 15,
     backgroundColor: "white",
     borderWidth: 2,
     borderColor: "#CEA4E2",
+    position: 'absolute',
+    left: 25,
+    top: 570,
   },
 
   loginButtonText: {
     fontWeight: "bold",
     color: '#CEA4E2',
-    fontFamily: 'Roboto',
   },
 
   signUpButton: {
-    width: "80%",
+    width: "42%",
     borderRadius: 10,
-    height: 50,
+    height: 45,
     alignItems: "center",
     justifyContent: "center",
     backgroundColor: "#9A6BAB",
+    position: 'absolute',
+    right: 25,
+    top: 570,
   },
 
   signUpButtonText: {
+    fontWeight: "bold",
+    color: 'white',
+  },
+
+  /* LOGIN PAGE -----------------------------*/
+  container2: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  loginTitle: {
+    fontSize: 30,
+    fontFamily: 'Comfortaa_400Regular',
+    marginBottom: 20,
+  },
+
+  loginInput: {
+    width: "80%",
+    backgroundColor: "white",
+    borderColor: "black",
+    marginBottom: 10,
+  },
+
+  loginButton2: {
+    width: "80%",
+    marginTop: 20,
+    borderRadius: 30,
+    height: 45,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#9A6BAB",
+    marginBottom: 10,
+  },
+
+  loginButtonText2: {
     fontWeight: "bold",
     color: 'white',
   },
